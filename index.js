8442c4ee14bef6f110d22cf82a2ffffbb2cc3c96
Quratulain <123683272+Quratulain209@users.noreply.github.com>
2023-08-02 02:56:04 -0700

    CORS

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 const port = process.env.SERVER_PORT
-
 app.use(express.json())
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        credentials: true,
+    })
+);
+
 app.use('/api', require('./API/Users/Routers'))
 app.use('/api', require('./API/Products/Routers'))
 app.use('/api', require('./API/Category/Routers'))
